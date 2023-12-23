9cc454a4c29cfbc12567855dccdb8adc70580a83
Prayas <prayasrajbgs28@gmail.com>
2023-12-23 09:22:39 +0530

    A drawing pad

---
+++ etch_ui.js
@@ -0,0 +1,49 @@
+const container = document.createElement('div');
+container.setAttribute("style", "display: block;");
+
+const resetButton = document.createElement('button');
+resetButton.textContent = "Reset";
+resetButton.addEventListener('click', function() {
+    let n = prompt("Enter a number between 1 and 100");
+    if (n < 1 || n > 100) {
+        alert("Invalid input. Please enter a number between 1 and 100");
+    } else {
+        container.innerHTML = "";
+        createGrid(n);
+    }
+});
+
+document.body.appendChild(resetButton);
+
+function createGrid(n) {
+    for (let rowIndex = 0; rowIndex < n; rowIndex++) {
+        const rowContainer = document.createElement('div');
+        rowContainer.setAttribute("style", "display: flex; flex-wrap: wrap;");
+    
+        for (let colIndex = 0; colIndex < n; colIndex++) {
+            let div = document.createElement('div');
+            div.setAttribute("style", "width: 25px; height: 25px; border: 1px solid black;");
+            
+            let randomR = Math.floor(Math.random() * 256);
+            let randomG = Math.floor(Math.random() * 256);
+            let randomB = Math.floor(Math.random() * 256);
+    
+            div.addEventListener('mouseover', function() {
+                div.style.backgroundColor = `rgb(${randomR}, ${randomG}, ${randomB})`;
+            });
+            rowContainer.appendChild(div);
+        }
+    
+        container.appendChild(rowContainer);
+    }
+    
+    document.body.appendChild(container);
+}
+
+// Function to create the initial grid with a default value of 16
+function initializeGrid() {
+    createGrid(16);
+}
+
+// Call the function to initialize the grid
+initializeGrid();
